6572f2e9c580c5a70b675a57824cfb3656655238
KAWANO Keisuke <141245826+kawano8942@users.noreply.github.com>
2023-08-05 12:34:59 +0900

    Update test_main.sh

--- test_main.sh
+++ test_main.sh
@@ -13,11 +13,24 @@ function test_gcd {
 
 # 引数が少ないケースのテスト
 echo "引数が少ないケースのテスト"
-./main.sh 2 || echo "エラー: 引数が少ない場合にエラーメッセージが出なかった"
+result=$(./main.sh 2)
+expected="引数の数が正しくありません。2つの自然数を指定してください。"
+if [ "$result" == "$expected" ]; then
+  echo "引数が少ないケースのテスト成功"
+else
+  echo "エラー: 引数が少ない場合にエラーメッセージが出なかった"
+  exit 1
+fi
 
 # 引数が多いケースのテスト
 echo "引数が多いケースのテスト"
-./main.sh 2 3 4 || echo "エラー: 引数が多い場合にエラーメッセージが出なかった"
+result=$(./main.sh 2 3 4)
+if [ "$result" == "$expected" ]; then
+  echo "引数が多いケースのテスト成功"
+else
+  echo "エラー: 引数が多い場合にエラーメッセージが出なかった"
+  exit 1
+fi
 
 # さまざまなテストケースでテスト
 echo "通常のケースのテスト"
@@ -27,12 +40,24 @@ test_gcd 121 11 11
 
 # 負の数のテスト
 echo "負の数のケースのテスト"
-./main.sh -2 4 || echo "エラー: 負の数が入力された場合にエラーメッセージが出なかった"
+result=$(./main.sh -2 4)
+expected="引数は正の整数である必要があります。"
+if [ "$result" == "$expected" ]; then
+  echo "負の数のケースのテスト成功"
+else
+  echo "エラー: 負の数が入力された場合にエラーメッセージが出なかった"
+  exit 1
+fi
 
 # 小数のテスト
 echo "小数のケースのテスト"
-./main.sh 2.5 4 || echo "エラー: 小数が入力された場合にエラーメッセージが出なかった"
+result=$(./main.sh 2.5 4)
+if [ "$result" == "$expected" ]; then
+  echo "小数のケースのテスト成功"
+else
+  echo "エラー: 小数が入力された場合にエラーメッセージが出なかった"
+  exit 1
+fi
 
 # 全てのテストがパスした
 echo "全てのテストが成功しました！"
-
